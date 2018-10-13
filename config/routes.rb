74a5eea50a5275feec93396724a370a617be9ce9
Justin Adkins <justin.adkins@mythcoders.com>
2018-10-13 13:36:23 -0400

    Notebooks controller and specs

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@
 
 Rails.application.routes.draw do
   devise_for :users
+
   root to: 'home#index'
+
+  resources :notebooks
 end
---
+++ spec/controllers/notebooks_controller_spec.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe NotebooksController, type: :controller do
+  describe '#index' do
+    it 'should show' do
+      get 'index'
+      expect(response.status).to be 200
+    end
+  end
+end
